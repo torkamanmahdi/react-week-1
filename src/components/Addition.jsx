da9a2d4d6d8d4a352be4d854cecaa90ce4130d91
Mahdi Torkaman <mahditorkaman@pm.me>
2022-02-01 12:23:36 +0330

    final

--- src/components/Addition.jsx
+++ src/components/Addition.jsx
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
-
+import "./Addition.css";
 class Addition extends Component {
   state = {
-    num1: parseInt(Math.random() * 10),
-    num2: parseInt(Math.random() * 10),
+    num1: 0,
+    num2: 0,
     response: 0,
     score: 0,
   };
-  changeScore = () => {
-    this.setState({ score: 1 });
-  };
+  changeState() {
+    let number1 = parseInt(Math.random() * 10);
+    let number2 = parseInt(Math.random() * 10);
+    let totalNumber = number1 + number2;
+    this.setState({
+      num1: number1,
+      num2: number2,
+      response: totalNumber,
+      score: 0,
+    });
+  }
+  upperScore() {
+    this.setState({
+      score: this.state.score + 1,
+    });
+    let getInput = document.querySelector(".guessNumber");
+    getInput.style.border = "4px solid green";
+    getInput.value = "";
+  }
+  downerScore() {
+    this.setState({
+      score: this.state.score - 1,
+    });
+    let getInput = document.querySelector(".guessNumber");
+    getInput.style.border = "4px solid red";
+    getInput.value = "";
+  }
   inputKeyPress(event) {
-    if (event.key === "Enter") {
-      if (1 === parseInt(document.querySelector(".guessNumber").value)) {
-        this.changeScore();
-      } else {
-        alert("wrong");
+    let inputValue = parseInt(document.querySelector(".guessNumber").value);
+    if (event.key === "Enter" && inputValue !== null) {
+      this.changeState();
+      if (this.state.response === inputValue) {
+        this.upperScore();
+      } else if (this.state.response !== inputValue) {
+        this.downerScore();
+      }
+      if (this.state.score === 9) {
+        alert("you are win, Hoooooooora");
+        this.setState({
+          score: 0,
+        });
       }
     }
   }
@@ -28,8 +60,9 @@ class Addition extends Component {
           </div>
           <input
             type="text"
-            className="guessNumber border-2 border-black rounded-md p-2 mt-5"
-            onKeyPress={this.inputKeyPress}
+            className="guessNumber border-4 border-black rounded-md p-2 mt-5"
+            onKeyPress={this.inputKeyPress.bind(this)}
+            value={this.inputValue}
             placeholder="Enter Number"
           />
         </div>
